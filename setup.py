ad16905236d66a07f4b4410050d5657d9397bb08
duanhongyi <duanhongyi@sunmap.com>
2013-12-12 16:01:16 +0800

    change version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_file_contents(filename):
         return fp.read()
 
 setup(name='poolbase',
-    version='0.1.2',
+    version='0.1.3',
     description="A connection pool for anything",
     long_description=get_file_contents('README.rst'),
     author="duanhongyi",
